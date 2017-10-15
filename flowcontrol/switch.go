fb5a40ec1174a73debe43b27b7138c1d9451509f
Yiheng Shu <lzw429@qq.com>
2017-10-15 17:03:49 +0800

    Update

---
+++ flowcontrol/switch.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("macOS.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.", os)
+	}
+}
